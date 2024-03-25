91baf484397dbf2329de1299f68f4755ef050d7e
Mariia2308 <136304734+Mariia2308@users.noreply.github.com>
2024-03-25 14:07:01 +0200

    add: deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: "/goit-react-hw-03/",
 plugins: [react()],
 build: {
 sourcemap: true,
